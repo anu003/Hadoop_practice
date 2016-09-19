86d5841ef6e2931f2a1cdbf6dee5629351715b05
Anusha Addagudi <anustudies.003@gmail.com>
2016-09-19 01:23:35 -0600

    Create mapper.py
    added mapper.py

---
+++ Hits_from_Page/mapper.py
@@ -0,0 +1,7 @@
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split()
+    if len(data) == 10:
+        ip, clientId, userName, time, timeZone, _, requestLine, _, statusCode, objectSize = data
+        print ip
